9a73d8cf4218f00f2e62541d6372ff9552290a44
Jami Äijänen <jami.aijanen@hotmail.com>
2022-02-13 15:58:35 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import React, { useDebugValue } from 'react';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [valinta, setValinta] = useState('USD');
+  const [rahaSumma, setRahaSumma] = useState('');
+  const [vastaus, setVastaus] = useState('');
+  
+  const laske = () => {
+    fetch(`http://api.exchangeratesapi.io/latest?access_key=9159b3aaf7405dfc09e1dd62e6af3880&symbols=${valinta}`)
+    .then(res => res.json())
+    .then(data => {
+      setVastaus((rahaSumma * Object.values(data.rates)).toFixed(2) + ' €')
+    })
+    .catch(error => {
+      Alert.alert('Error', error);
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.vastaus}>{vastaus}</Text>
+      <TextInput placeholder='€' onChangeText={ text => setRahaSumma(text) } />
+      <Picker
+        selectedValue={valinta}
+        style={{ height: 50, width: 150}}
+        onValueChange={(value, index) => setValinta(value)}
+      >
+        <Picker.Item label="USD" value="USD" />
+        <Picker.Item label="GBP" value="GBP" />
+        <Picker.Item label="JPY" value="JPY" />
+        <Picker.Item label="AUD" value="AUD" />
+        <Picker.Item label="CHF" value="CHF" />
+      </Picker>
+      <Button title="Convert" onPress={laske} />
       <StatusBar style="auto" />
     </View>
   );
